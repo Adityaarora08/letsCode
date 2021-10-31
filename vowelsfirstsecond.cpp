acaff962812a5154ffaec1d193f67765dac41cd6
Adityaarora08 <79107244+Adityaarora08@users.noreply.github.com>
2021-10-31 23:44:46 +0530

    Add files via upload

---
+++ vowelsfirstsecond.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void vowelwords()
+{
+ifstream fin;
+fin.open("FIRST.TXT");
+ofstream fout;
+fout.open("SECOND.TXT");
+char word[30];
+while(!fin.eof())
+{
+fin>>word;
+if(word[0]=='a'||word[0]=='e'||word[0]=='i'||word[0]=='o'||word[0]=='u')
+fout<<word<<" ";
+}
+fin.close();
+fout.close();
+
+}
