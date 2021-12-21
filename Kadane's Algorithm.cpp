831a1ff005ba9462ff4fc77d5e39815b61884708
Adityaarora08 <79107244+Adityaarora08@users.noreply.github.com>
2021-12-21 22:50:51 +0530

    Add files via upload

---
+++ Kadane's Algorithm.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int kadane(int arr[],int n)
+{
+	int start_sum=0;
+	int current_sum=0;
+	for(int i=0;i<n;i++)
+	{
+		current_sum+=arr[i];
+		if(start_sum<current_sum)
+		start_sum=current_sum;
+		if(current_sum<0)
+		current_sum=0;
+	}
+}
