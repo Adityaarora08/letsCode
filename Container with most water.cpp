e87547991f831a9e8ce8a7550f146a0518224587
Aditya <79107244+Adityaarora08@users.noreply.github.com>
2023-07-19 23:40:05 +0530

    Update Container with most water.cpp

--- Container with most water.cpp
+++ Container with most water.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // int l= sizeof(height)/sizeof(height[0]);
-        // int area=0;
-        // for(int i=0;i<l;i++)
-        // {
-        //     if(height[i]>=height[l])
-        //     {
-        //         int a=l-i-1;
-        //         area=height[l]*a;
-        //         break;
-        //     }
-        // }
-        // return area;
-int s=0,e=height.size()-1;
-int m=0;
-while(s<e){
-m=max(m,min(height[s],height[e])*(e-s));
-if(height[s]<height[e])
-    s++;
-else 
-    e--;
-}
-return m;
+        int l=0,h=height.size()-1,maxi=INT_MIN;
+        while(l<h){
+            maxi=max(maxi,min(height[l],height[h])*(h-l));
+            if(height[l]>height[h])h--;
+            else l++;
+        }
+        return maxi;
     }
 };
